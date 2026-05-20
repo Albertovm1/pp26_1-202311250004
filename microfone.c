1bcfea58b2770a7074f8cc3a2064340b9deede05
Alberto Viturino Mamede <albertoviturino99@gmail.com>
2026-05-19 22:28:53 -0300

    Primeiro commit

---
+++ microfone.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "microfone.h"
+
+Microfone* criar_microfone(int id) {
+    Microfone* m = (Microfone*)malloc(sizeof(Microfone));
+    if (m) {
+        m->id = id;
+        m->tempo_limite = 60; // Agora o compilador conhece esses membros
+    }
+    return m;
+}
+
+void configurar_tempo(Microfone* m, int segundos) {
+    if (m) {
+        m->tempo_limite = segundos;
+    }
+}
+
+int ligar_microfone(Microfone* m) {
+    if (!m) return 0;
+    printf("Microfone %d ligado por %d segundos.\n", m->id, m->tempo_limite);
+    return 1;
+}
